e291885501c84e3a1ba0195faaa4839fdbcb8629
hecha2 <121083956+hecha2@users.noreply.github.com>
2020-08-23 15:27:07 +0000

    reminder

--- Alinea/AppDelegate.swift
+++ Alinea/AppDelegate.swift
@@ -17,4 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
-   
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIA
