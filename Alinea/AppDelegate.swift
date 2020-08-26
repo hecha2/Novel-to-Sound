557fa2490da4515652d3feb512a2414150500fcc
hecha2 <121083956+hecha2@users.noreply.github.com>
2020-08-26 19:28:08 +0000

    Update npm dependencies

--- Alinea/AppDelegate.swift
+++ Alinea/AppDelegate.swift
@@ -27,4 +27,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessi
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // 
