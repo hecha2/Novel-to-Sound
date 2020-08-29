a940b03053bd0b836773f71abaf2f04e02a670ea
hecha2 <121083956+hecha2@users.noreply.github.com>
2020-08-29 09:26:47 +0000

    fixed markdown syntax

--- Alinea/AppDelegate.swift
+++ Alinea/AppDelegate.swift
@@ -33,4 +33,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-    // M
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer =
