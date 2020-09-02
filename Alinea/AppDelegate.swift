ad100713498e91193a8c73b297872eda39f6d4c0
hecha2 <121083956+hecha2@users.noreply.github.com>
2020-09-02 14:16:53 +0000

    dm btree: fix leak of bufio-backed block in btree_split_sibling error path

--- Alinea/AppDelegate.swift
+++ Alinea/AppDelegate.swift
@@ -46,4 +46,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
-                // fatalError() 
+                // fatalError() causes the application to generate a crash log and terminate. You should not use
