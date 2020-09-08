3e60e2ca768044a8390fbe5725aa16a17088803e
hecha2 <121083956+hecha2@users.noreply.github.com>
2020-09-08 18:54:18 +0000

    Version 1.4.1

--- Alinea/AppDelegate.swift
+++ Alinea/AppDelegate.swift
@@ -71,4 +71,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 try context.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may
