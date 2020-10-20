2c92e043f21a0e563848fdaf1ca305660b233d81
hecha2 <121083956+hecha2@users.noreply.github.com>
2020-10-20 13:18:09 +0000

    !

--- Alinea/Extensions/UIColor+Extension.swift
+++ Alinea/Extensions/UIColor+Extension.swift
@@ -13,4 +13,5 @@ extension UIColor {
     convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
         self.init(
             red: CGFloat(red) / 255.0,
-  
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue
