aff421416b938b2e31e9d29cd66a1d7119e2ee25
hecha2 <121083956+hecha2@users.noreply.github.com>
2022-01-16 10:06:26 +0000

    Work in Progress

--- Alinea/SceneDelegate.swift
+++ Alinea/SceneDelegate.swift
@@ -43,4 +43,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transiti
+        // Called as the scene transitions from the foreground to the background.
+        // Use this me
