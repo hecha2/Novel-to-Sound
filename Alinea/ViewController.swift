e672455e24fae20e1750b3df3bd4a76777f27889
hecha2 <121083956+hecha2@users.noreply.github.com>
2022-01-26 13:51:25 +0000

    edited index.html

--- Alinea/ViewController.swift
+++ Alinea/ViewController.swift
@@ -12,4 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any add
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
