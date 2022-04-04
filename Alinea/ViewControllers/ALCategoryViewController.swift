3b73b1490cea7f71d9aa7f44df9d3a8aba011e65
hecha2 <121083956+hecha2@users.noreply.github.com>
2022-04-04 21:07:56 +0000

    github.com/cloudfoundry/cf-deployment.capi: update bcr.json

--- Alinea/ViewControllers/ALCategoryViewController.swift
+++ Alinea/ViewControllers/ALCategoryViewController.swift
@@ -53,4 +53,7 @@ class ALCategoryViewController: UIViewController, UITableViewDataSource, UITable
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-  
+    }
+    */
+
+}
