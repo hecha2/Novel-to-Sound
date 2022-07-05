75e78251267fad7d989f10b751c723716ecb3771
hecha2 <121083956+hecha2@users.noreply.github.com>
2022-07-05 18:15:39 +0000

    Incremented version number.

--- Alinea/ViewControllers/ALMainViewController.swift
+++ Alinea/ViewControllers/ALMainViewController.swift
@@ -75,4 +75,7 @@ class ALMainViewController: UIViewController {
         changeIconColor(color: UIColor.lightGray, image: GROUP_ICON, imageView: fourthViewIcon)
         changeIconColor(color: UIColor.lightGray, image: BULB_ICON, imageView: fifthViewIcon)
         changeIconColor(color: UIColor.lightGray, image: TRADE_ICON, imageView: thirdViewIcon)
-        changeIconColor(color: UIColor.white, image: FILE_ICON, imageView: fileViewIcon
+        changeIconColor(color: UIColor.white, image: FILE_ICON, imageView: fileViewIcon)
+        let previousIndex = selectedIndex
+        selectedIndex = 0
+     
