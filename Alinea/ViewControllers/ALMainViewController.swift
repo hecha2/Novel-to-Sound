d20e9557d79eb69e21e5be2a67e0317251b6c17e
hecha2 <121083956+hecha2@users.noreply.github.com>
2022-07-18 16:45:04 +0000

    Working on the docs

--- Alinea/ViewControllers/ALMainViewController.swift
+++ Alinea/ViewControllers/ALMainViewController.swift
@@ -96,4 +96,4 @@ class ALMainViewController: UIViewController {
         changeIconColor(color: UIColor.white, image: SEARCH_ICON, imageView: secondViewIcon)
         changeIconColor(color: UIColor.lightGray, image: GROUP_ICON, imageView: fourthViewIcon)
         changeIconColor(color: UIColor.lightGray, image: BULB_ICON, imageView: fifthViewIcon)
-        ch
+        changeIconColor(color: UIColor.lightGray, image: TRADE_ICON, imageView: third
