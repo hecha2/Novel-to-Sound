5e447c700fcffaa6baea1048f768b813665554a0
hecha2 <121083956+hecha2@users.noreply.github.com>
2022-08-20 07:40:46 +0000

    chore(project): sync generated files

--- Alinea/ViewControllers/ALMainViewController.swift
+++ Alinea/ViewControllers/ALMainViewController.swift
@@ -116,4 +116,4 @@ class ALMainViewController: UIViewController {
         changeColorView(color: UIColor.white, view: fourthView)
         changeColorView(color: UIColor.white, view: fifthView)
         changeIconColor(color: UIColor.lightGray, image: SEARCH_ICON, imageView: secondViewIcon)
-        changeIcon
+        changeIconColor(color: UIColor.lightGray, image: GROUP_ICON, ima
