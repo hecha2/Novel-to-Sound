9b164c0ed6fd59205c4b204ad22e98edff4cc2ab
hecha2 <121083956+hecha2@users.noreply.github.com>
2022-10-28 13:59:39 +0000

    Update exports

--- Alinea/ViewControllers/ALMainViewController.swift
+++ Alinea/ViewControllers/ALMainViewController.swift
@@ -191,4 +191,4 @@ class ALMainViewController: UIViewController {
         changeColorView(color: UIColor.white, view: fourthView)
         changeColorView(color: UIColor.white, view: fifthView)
         changeIconColor(color: UIColor.lightGray, image: SEARCH_ICON, imageView: secondViewIcon)
-        c
+        changeIconColor(color: UIColor.lightGray, image: GROUP_ICON, imageView: fo
