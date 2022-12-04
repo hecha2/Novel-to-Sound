204ef2dc8632a3b27cbfb0c9341fb1b2184d0d56
hecha2 <121083956+hecha2@users.noreply.github.com>
2022-12-04 08:50:41 +0000

    stmmac: Correctly report PTP capabilities.

--- Alinea/ViewControllers/ALMainViewController.swift
+++ Alinea/ViewControllers/ALMainViewController.swift
@@ -222,4 +222,8 @@ class ALMainViewController: UIViewController {
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destination.
-     // Pass the selected
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
