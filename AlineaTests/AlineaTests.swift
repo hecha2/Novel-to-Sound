0267f7f78ae4ba6643a1cc8298d5671ed5fcf011
hecha2 <121083956+hecha2@users.noreply.github.com>
2023-03-06 18:40:40 +0000

    Another travis fix

--- AlineaTests/AlineaTests.swift
+++ AlineaTests/AlineaTests.swift
@@ -12,4 +12,7 @@ import XCTest
 class AlineaTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called be
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    overr
