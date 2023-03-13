084a713621995b6c9e390049be4764630485b1e1
hecha2 <121083956+hecha2@users.noreply.github.com>
2023-03-13 18:08:40 +0000

    regmap: fix possible ZERO_SIZE_PTR pointer dereferencing error.

--- AlineaTests/AlineaTests.swift
+++ AlineaTests/AlineaTests.swift
@@ -17,4 +17,7 @@ class AlineaTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-   
+    }
+
+    func testExample() throws {
+        // This is an example of a functional te
