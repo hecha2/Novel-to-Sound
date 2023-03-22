783774ed4d77b91eba26a5db61967884479ab854
hecha2 <121083956+hecha2@users.noreply.github.com>
2023-03-22 18:18:36 +0000

    remove some files

--- AlineaUITests/AlineaUITests.swift
+++ AlineaUITests/AlineaUITests.swift
@@ -13,4 +13,5 @@ class AlineaUITests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In UI tests it is usually best
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        contin
