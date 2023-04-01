5f613b344a714c9307cfd12d90dab40ddb2831f8
hecha2 <121083956+hecha2@users.noreply.github.com>
2023-04-01 10:00:07 +0000

    windoproj

--- AlineaUITests/AlineaUITests.swift
+++ AlineaUITests/AlineaUITests.swift
@@ -29,4 +29,7 @@ class AlineaUITests: XCTestCase {
         app.launch()
 
         // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to ve
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func
