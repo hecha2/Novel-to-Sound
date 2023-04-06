3ba6d311b6f5efa6b0f1d39a6b1263a48afa9f44
hecha2 <121083956+hecha2@users.noreply.github.com>
2023-04-06 17:21:07 +0000

    Auto Upload

--- AlineaUITests/AlineaUITests.swift
+++ AlineaUITests/AlineaUITests.swift
@@ -33,4 +33,5 @@ class AlineaUITests: XCTestCase {
     }
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch y
