616ac430fcef2e4f0db86de7f5179a6e6334a166
hecha2 <121083956+hecha2@users.noreply.github.com>
2023-04-07 21:54:29 +0000

    new gems

--- AlineaUITests/AlineaUITests.swift
+++ AlineaUITests/AlineaUITests.swift
@@ -35,4 +35,8 @@ class AlineaUITests: XCTestCase {
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunc
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    
